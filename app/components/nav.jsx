8847665708b381d2ce3828da92de49d1839e15ba
Magister99 <jpalaciosmendias0@gmail.com>
2025-01-19 02:36:47 -0600

    Pagina principal JVSyC

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,6 +1,7 @@
 export default function Nav() {
     return (
       <div className="bg-red-700 flex items-center justify-between p-4">
+
         
         <h1 className="text-white text-xl font-bold mx-auto">SISTEMA TITULACIÓN</h1>
         
